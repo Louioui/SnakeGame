7e7322616b979c1ad7c767c07937e5d9aca760f5
luis-pacheco <luispacheco13a@gmail.com>
2022-07-23 01:55:39 -0700

    changing css style

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameboard.getContext("2d");
+const ctx = gameBoard.getContext("2d");
 const scoreText= document.querySelector("#scoreText");
 const resetBtn= document.querySelector("#resetBtn");
 
@@ -15,6 +15,13 @@ let yVelocity = 0;
 let foodX;
 let foodY;
 let score = 0;
+
+
 let snake = [
-    //code
+
+    {x: unitSize * 4, y:0},
+    {x: unitSize * 3, y:0},
+    {x: unitSize * 2, y:0},
+    {x: unitSize, y:0},
+    {x:0, y:0}  
 ]
